37be0511b038bddd50c4037367537c4a2ed2ff13
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-12-05 18:05:52 -0300

    Desenvolvcer as rotas no angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ConteudoComponent } from './pages/conteudo/conteudo.component';
+import { HistoriaComponent } from './pages/historia/historia.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { ContatoComponent } from './pages/contato/contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'conteudo', component: ConteudoComponent },
+  { path: 'historia', component: HistoriaComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'contato', component: ContatoComponent }
+
+    
+];
--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -5,7 +5,7 @@ import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { map, shareReplay } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
@@ -19,7 +19,8 @@ import { CommonModule } from '@angular/common';
     MatIconModule,
     MatListModule,
     MatButtonModule,
-    RouterOutlet],
+    RouterOutlet,
+    RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
