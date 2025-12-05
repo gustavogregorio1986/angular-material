407474b6f3315164616951018216da4272f2c205
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-12-05 17:15:28 -0300

    Inicializar o projeto

---
+++ src/app/pages/menu/menu.component.ts
@@ -0,0 +1,51 @@
+// app.module.ts
+import { Component, ViewChild } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterOutlet } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, shareReplay } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-menu',
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    RouterOutlet],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+     @ViewChild('sidenav') sidenav!: MatSidenav;
+
+     isMobile = false;
+     
+     constructor(private bp: BreakpointObserver) {
+          this.bp
+      .observe([Breakpoints.Handset])
+      .pipe(
+        map(result => result.matches),
+        shareReplay()
+      )
+      .subscribe(isHandset => {
+        this.isMobile = isHandset;
+        if (!isHandset) {
+          this.sidenav?.open();
+        }
+      });
+     }
+
+      toggleSidenav() {
+    if (this.isMobile) this.sidenav.toggle();
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+export default function bootstrap(context: any) {
+  return bootstrapApplication(AppComponent, appConfig, context);
+}
